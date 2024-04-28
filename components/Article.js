ed76814e79621b7c6614a59cfb1b3783790e03d8
hkyuuu00 <lhk3121@gmail.com>
2024-04-29 01:32:16 +0900

    test

--- components/Article.js
+++ components/Article.js
@@ -5,7 +5,7 @@ import styles from "../styles";
 export default function Article() {
   return (
     <View style={[styles.article, styles.content]}>
-      <Text>JaeHwi8cm</Text>
+      <Text>JaeHwi</Text>
     </View>
   );
 }
