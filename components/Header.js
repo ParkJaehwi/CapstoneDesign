70426f4a355d8d767aca748f3d017cb48af8234f
hkyuuu00 <lhk3121@gmail.com>
2024-04-29 03:32:46 +0900

    header,footer 기본디자인

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button, TouchableOpacity, onPress } from "react-native";
 import styles from "../styles";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function Header() {
   return (
     <View style={[styles.header, styles.content]}>
-      <Text style={styles.whiteText}>header</Text>
+      <View style={styles.headerItem}>
+        <TouchableOpacity onPress={onPress} style={styles.button}>
+          <Icon name="bars" size={24} color="black" />
+        </TouchableOpacity>
+        <Text style={[styles.whiteText,styles.headerText]}>BU Market</Text>
+        <TouchableOpacity onPress={onPress} style={styles.button}>
+          <Icon name="search" size={24} color="black" />
+        </TouchableOpacity>
+      </View> 
     </View>
   );
 }
